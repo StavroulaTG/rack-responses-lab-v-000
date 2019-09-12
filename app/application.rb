6462c038e6946ba8734526e2cab342fca2c9b67b
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-12 01:19:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    if
+      resp.write "Good morning"
+    else
+      resp.write "Good afternoon"
+    end
   end
   resp.finish
 end
