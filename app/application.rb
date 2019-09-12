5fd04952c03175792e9bae3a31a7e754d4fa67f1
Stavroula Guerrero <4stacyg@optonline.net>
2019-09-12 01:22:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if Time.new
+    if Time.new.hour
+      
       resp.write "Good morning"
     else
       resp.write "Good afternoon"
